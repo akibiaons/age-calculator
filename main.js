3a5b242d44e1a32baeb7e8cdfde1f5580579e924
Arthur Fernandez <arthur@snooget.com>
2023-04-10 21:09:29 -0700

    Capitalized syntax

--- main.js
+++ main.js
@@ -87,7 +87,7 @@ SubmitBtn.addEventListener ('click', function (e) {
     let month = CheckMonthInput();
     let year = CheckYearInput();
     if (!day || !month || !year) return 
-    let birthday = `${inputMonth.value}/${InputDay.value}/${InputYear.value}`;
+    let birthday = `${InputMonth.value}/${InputDay.value}/${InputYear.value}`;
     calculateAge(birthday);
     Canvas.style.display = 'block';
     setTimeout(function () {
